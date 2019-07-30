5366acea3b127443961e04efd4c1b1b395158c4e
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-30 10:10:37 -0600

    Inital setup for cook_book class

---
+++ test/cook_book_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/pantry'
+require './lib/cook_book'
+require 'pry'
+
+class Cook_BookTest < MiniTest::Test
+  def setup
+    @cookbook = CookBook.new
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+    @ground_beef = Ingredient.new("Ground Beef", "oz", 100)
+    @bun = Ingredient.new("Bun", "g", 1)
+    @burger = Recipe.new("Burger")
+    @burger.add_ingredient(@ground_beef, 4)
+    @burger.add_ingredient(@bun, 100)
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+end
